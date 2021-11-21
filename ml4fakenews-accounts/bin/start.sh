9d6ef983a88a975ec9f2b0aaa2115e2ba3177e88
Marek Maras <marek@niceguys.info>
2021-11-21 23:14:39 +0100

    Added APM

--- ml4fakenews-accounts/bin/start.sh
+++ ml4fakenews-accounts/bin/start.sh
@@ -2,6 +2,8 @@
 
 # CONFIGURE AND START DATADOG AGENT IF API KEY PROVIDED
 if [ -n "$DD_API_KEY" ]; then
+  wget -O /dd-java-agent.jar 'https://dtdg.co/latest-java-tracer'
+
   DD_INSTALL_ONLY=true DD_AGENT_MAJOR_VERSION=7 DD_SITE="datadoghq.com" \
     bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script.sh)"
 
@@ -19,6 +21,7 @@ logs:
 EOM
 
   service datadog-agent start
+  java -javaagent:/dd-java-agent.jar -Ddd.service=account -Ddd.env=prod -jar /app.jar
+else
+  java -jar /app.jar
 fi
-
-java -jar /app.jar
